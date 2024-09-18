cf80c43513fcfe6e78b6e17fdd34368892ec0308
Prakhar <prakhar102004@gmail.com>
2024-09-18 08:31:25 +0530

    writting code for scroll trigger and making it responsive

---
+++ scroll-trigger/main.js
@@ -0,0 +1,14 @@
+const cards = document.querySelectorAll(".card");
+cards.forEach((card) => {
+  gsap.to(card, {
+    scale: 0.7,
+    opacity: 0,
+    scrollTrigger: {
+      trigger: card,
+      start: "top 15%",
+      end: "bottom 15%",
+    //   markers: true,
+      scrub: true,
+    },
+  });
+});
